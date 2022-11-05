41f0499d1135935221b22d994a6e3220bfa73d55
yayner <yayner2002@gmail.com>
2022-11-05 17:42:20 +0300

    Add axios and use it inside tasklist component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class TaskList extends Component {
-  state = {  } 
+  constructor(props) {
+    super(props)
+
+    this.deleteTask = this.deleteTask.bind(this)
+    this.state = {
+      tasks: []
+    }
+  }
+
+  componentDidMount() {
+    const endPoint = `http://localhost:4000/tasks/`
+    axios.get(endPoint)
+    .then((res) => {
+      this.setState({
+        tasks: res.data
+      })
+    })
+    .catch((err) => console.log(err))
+  }
+
+  deleteTask() {
+
+  }
   render() { 
     return (
     <div>
-      <p>wellcome to the task list component</p>
+      
     </div>
 
     );
