38944a1320b50f5c667b66ff80460f5f61ea864e
yayner <yayner2002@gmail.com>
2022-11-05 15:49:32 +0300

    Implement the add user component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
 
 class CreateUser extends Component {
-  state = {  } 
+  constructor(props) {
+    super(props);
+
+    this.onchangeUsername = this.onchangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  onchangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    console.log(user);
+
+
+    this.setState({
+      username:''
+    })
+  }
+
+
+
+
   render() { 
     return (
     <div>
-      <p>wellcome to the create user component</p>
+      <h3>Create a New User</h3>
+      <form onSubmit={this.onSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type="text"  onChange={this.onchangeUsername}
+              value={this.state.username} required className='form-control' />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create User" className='btn btn-primary' />
+        </div>
+      </form>
     </div>
 
     );
